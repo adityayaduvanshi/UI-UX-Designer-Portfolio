bd4d210a0f7ec8f7b277588f591f12ce79dad97e
Aditya Yaduvanshi <aditya97y@gmail.com>
2024-01-04 08:25:42 +0530

    addded

--- app/(main)/about/page.jsx
+++ app/(main)/about/page.jsx
@@ -22,12 +22,12 @@ const page = () => {
   }, []);
   return (
     <>
-      <div className="mt-4 overflow-x-hidden ">
+      <div className="mt-4 block  w-[100vw]   ">
         <motion.div
           initial={{ opacity: 0, y: 50 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.6 }}
-          className="bg-white flex flex-row gap-20 mt-8 justify-center items-center w-[100vw] px-10"
+          className="bg-white flex flex-row gap-20 mt-8 mx-10 justify-center items-center w-[100vw] px-10"
         >
           <div className="flex relative">
             <div className=" absolute top-0 right-60 [font-family:'Montserrat-Bold',Helvetica] font-bold text-[#c85a74] text-[115px] tracking-[0] leading-[138.0px]">
@@ -43,19 +43,19 @@ const page = () => {
           </div>
           <div className="">
             <p className=" w-[706px] [font-family:'Public_Sans-Medium',Helvetica] font-medium text-[#212b36] text-[24px] tracking-[0] leading-[35.0px]">
-              Hey there! <br />
-              I&#39;m a passionate UI/UX designer with a knack for creating
-              intuitive and delightful digital experiences. I kick-started my
-              journey during my final year of engineering at college when I
-              landed an internship at AUDI, where I had the privilege to hone my
-              skills in crafting user-centric designs for automotive interfaces.
-              Fast forward to today, with over 2 years of experience in the
-              field, I&#39;ve dived deep into the world of SAAS-based products,
-              crafting seamless interactions that make users&#39; lives easier.
-              My journey began by designing and developing websites and apps for
-              friends and family, and from there, I launched into the world of
-              freelancing, constantly seeking fresh challenges and innovative
-              solutions to make every user&#39;s experience extraordinary.
+              Hey there! <br />I m a passionate UI/UX designer with a knack for
+              creating intuitive and delightful digital experiences. I
+              kick-started my journey during my final year of engineering at
+              college when I landed an internship at AUDI, where I had the
+              privilege to hone my skills in crafting user-centric designs for
+              automotive interfaces. Fast forward to today, with over 2 years of
+              experience in the field, Ive dived deep into the world of
+              SAAS-based products, crafting seamless interactions that make
+              users lives easier. My journey began by designing and developing
+              websites and apps for friends and family, and from there, I
+              launched into the world of freelancing, constantly seeking fresh
+              challenges and innovative solutions to make every users experience
+              extraordinary.
             </p>
           </div>
         </motion.div>
@@ -63,7 +63,7 @@ const page = () => {
           initial={{ opacity: 0, y: 20 }} // Initial animation properties
           animate={{ opacity: 1, y: 0 }} // Animation properties
           transition={{ duration: 0.6 }}
-          className=" py-20  bg-[#fff2f3]"
+          className=" py-10  bg-[#fff2f3]"
         >
           <motion.div
             initial={{ opacity: 0 }} // Initial animation properties for child element
@@ -72,7 +72,7 @@ const page = () => {
             className="flex gap-8 flex-col justify-center"
           >
             <div>
-              <h4 className=" [font-family:'Montserrat-Regular',Helvetica] font-normal text-[#c85a74] text-[40px] text-center tracking-[0] leading-[48.0px] whitespace-nowrap">
+              <h4 className=" [font-family:'Montserrat-Regular',Helvetica] font-normal text-[#c85a74] text-[40px] text-center tracking-[0] leading-[48.0px] ">
                 Life Beyond Design
               </h4>
               <div className="flex mt-4 w-full justify-center">
@@ -81,7 +81,7 @@ const page = () => {
             </div>
             <div className="flex  justify-center">
               <div className=" w-[50%]  text-center  font-light text-[#637381] text-[14px]  ">
-                Outside the world of design, I'm a big fan of yoga and running –
+                Outside the world of design, I'm a big fan of yoga and running
                 they keep me grounded and energized. When I'm not chasing pixels
                 or perfecting user experiences, I'm chasing shuttlecocks on the
                 badminton court. Traveling is my ultimate escape, and I've been
